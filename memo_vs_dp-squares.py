97c463889ec2f25c8e93aa2d46a944d48c676f3b
Drew <Andrew@Andrews-MacBook-Pro.local>
2015-01-13 15:17:01 -0500

    memo vs dp - sum of squares

---
+++ memo_vs_dp-squares.py
@@ -0,0 +1,32 @@
+from time import time
+
+class memo:                                   # memoize
+    def __init__(self, fn):
+        self.fn, self.m = fn, {}
+    def __call__(self, *k):
+        if len(k)==1: k=k[0]
+        if k not in self.m: self.m[k] = self.fn(k)
+        return self.m[k]
+
+
+@memo
+def T(n):
+    if n==0:  return 0
+    return min( T(n-i*i) for i in xrange(int(n**.5),0,-1) ) + 1
+
+
+def DP(n):
+    T = [0]*(n+1)
+    for i in xrange(1, n+1):
+        T[i] = min ( T[i-x*x] for x in xrange(1,int(i**.5)+1) ) + 1
+    return T
+
+
+
+print '       n     DP   Memo'
+for n in [100, 1000, 10000, 100000, 1000000]:
+    sqs = [i*i for i in xrange(int(n**.5)+1)]
+    toc,_,tic = time(), DP(n), time()
+    toc2,_,tic2 = time(), T(n), time()
+    T.m.clear()
+    print str(n).rjust(8), str(tic-toc)[:6], str(tic2-toc2)[:6]
