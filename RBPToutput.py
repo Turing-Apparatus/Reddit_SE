01cc5128904d600bdc06cb24a8b8e4b916207a15
Drew <Andrew@Andrews-MacBook-Pro.local>
2015-02-05 01:02:04 -0500

    edit out

--- RBPToutput.py
+++ RBPToutput.py
@@ -27,7 +27,7 @@ THESE NUMBERS COUNT SUCH OCCURENCES
 
 
 _____________________________________________________________________
-FOR EACH (i,j) (k__x) COUNTS THE NUMBER OF WAYS TO GLUE TWO TREES
+(i,j) (k__x) COUNTS THE NUMBER OF WAYS TO GLUE TWO TREES
 IN T[i] AND T[j] SUCH THAT THE RESULT IS A TREE IN T[K]
 
 (1, 1) (2__1)
