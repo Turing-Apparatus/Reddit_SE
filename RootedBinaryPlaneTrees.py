445e66af8de1f4bc47dac636b3941cff92c49fac
Drew <Andrew@Andrews-MacBook-Pro.local>
2015-02-06 02:25:59 -0500

    tree

--- RootedBinaryPlaneTrees.py
+++ RootedBinaryPlaneTrees.py
@@ -3,27 +3,27 @@ from operator import add
 
 
 def glue(l,r):
-    global T
-    s = l[1] | r[1]
-    ns = len(s)
-    if ns < N:
-        t = 'x({},{})'.format(l[0],r[0])
-        T[ns+1].append( (t,s|set([t])) )
-        if l[0]!=r[0]:
-            t = 'x({},{})'.format(r[0],l[0])
-            T[ns+1].append( (t,s|set([t])) )
+    s = l | r
+    if len(s) < N:
+        next_tree(s)
+        next_tree(s)
 
-
+def next_tree(s):
+    global T, count
+    T[len(s)+1].append( s | set([count]) )
+    count += 1
 
 
 N = 7
 T = [[] for i in xrange(2*N)]
-T[1] = [('x',set('x'))]
+count = 1
+next_tree(set())
+
 
 for n in xrange(2,N+1):
     for l,r in product(T[n-1], reduce(add,T[1:n-1],[]) ):   glue(l,r)
     for l,r in combinations(T[n-1],2):                      glue(l,r)
-    for t in T[n-1]:                                        glue(t,t)
+    for t in T[n-1]:                                        next_tree(t)
     print n, len(T[n])
 
 
